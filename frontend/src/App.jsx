fdd8b6e91c85b7b63c97301576fd588bf4933b16
shrajan <shrajan@gmail.com>
2025-09-16 10:10:44 +0530

    feat(project): initialize project with React, Tailwind, and routing
    Set up base React project with React 19 and React DOM
    Added React Router DOM for client-side routing
    Configured TailwindCSS with Vite for styling
    Installed GSAP and GSAP React plugin for animations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import SignupPage from "./pages/authPages/SignupPage";
+import AuthPage from "./pages/authPages/AuthPage";
+import LoginPage from "./pages/authPages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <main className="min-h-dvh w-full">
+      {/* <Navbar /> */}
+
+      <Routes>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
