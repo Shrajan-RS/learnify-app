3ac47b65a49814aeb50067a1f32eee4dea12f3fb
shrajan <shrajan@gmail.com>
2025-09-16 10:07:02 +0530

    feat(modify): modified cors in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,13 @@ import errorHandler from "./middlewares/error.handler.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
